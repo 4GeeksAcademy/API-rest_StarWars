5d520fc910bc97492bfda94cf5c47ff08fc929d1
tomasmj503 <86091325+tomasmj503@users.noreply.github.com>
2024-01-29 22:24:06 +0000

    modelado y rutas

--- src/admin.py
+++ src/admin.py
@@ -1,16 +1,25 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import db, User, Character, Planet, Favorites #importar las tablas
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+                        #Nombre que aparece
+    admin = Admin(app, name='StarWars', template_mode='bootstrap3')
 
-    
+    #-----------SE CONFIGURA LAS TABLAS PARA VERLO GRAFICAMENTE -------------
+
+    #agregar las tablas de forma visual al modelo de la base de datos----------
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Character, db.session))
+    admin.add_view(ModelView(Planet, db.session))
+    admin.add_view(ModelView(Favorites, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
+
+
+    #ir al archivo app.py para importar las tablas
--- src/app.py
+++ src/app.py
@@ -8,8 +8,9 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Favorites
 #from models import Person
+import json
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -35,15 +36,68 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
+#colocar puerto publico
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_usuarios():
+    #acceda a la tabla user y traiga toda la info
+    todos = User.query.all()
+    if todos == []:
+        #cuando esta vacio jsonify me transforma en un objeto
+        return jsonify({"msg": "No hay usuarios"}), 404 
+                         #callback
+    resultado = list(map(lambda usuario: usuario.serialize(), todos))
+    return jsonify(resultado), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    #acceda a la tabla user y traiga toda la info
+    todos = Character.query.all()
+    if todos == []:
+        #cuando esta vacio jsonify me transforma en un objeto
+        return jsonify({"msg": "No hay personajes"}), 404 
+                         #callback
+    resultado = list(map(lambda character: character.serialize(), todos))
+    return jsonify(resultado), 200
+
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    #acceda a la tabla user y traiga toda la info
+    todos = Planet.query.all()
+    if todos == []:
+        #cuando esta vacio jsonify me transforma en un objeto
+        return jsonify({"msg": "No hay planetas"}), 404 
+                         #callback
+    resultado = list(map(lambda planet: planet.serialize(), todos))
+    return jsonify(resultado), 200
+
+@app.route('/favorites', methods=['GET'])
+def get_favorites():
+    #acceda a la tabla user y traiga toda la info
+    todos = Favorites.query.all()
+    if todos == []:
+        #cuando esta vacio jsonify me transforma en un objeto
+        return jsonify({"msg": "No hay favoritos"}), 404 
+                         #callback
+    resultado = list(map(lambda favorite: favorite.serialize(), todos))
+    return jsonify(resultado), 200
+
+@app.route('/favorites', methods=['POST'])
+def add_favorites():
+    body = json.loads(request.data) # traiga la info que solicito, importar despues json
+            #referencia a la tabla
+    nuevo_favorito = Favorites(
+        #se accede a las tablas a traves del modelo models.py
+        user_id = body["user_id"], # a traves del body capturamos el id usuario
+        planet_id = body["planet_id"],
+        character_id = body["character_id"]
+    )
+    db.session.add(nuevo_favorito)#agregue 
+    db.session.commit()
+    return jsonify({"msg": "Favorito creado"}), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
